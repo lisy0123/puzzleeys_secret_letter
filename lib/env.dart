369a2d218bb933592e539b3ad1f9b2f1fedc4bc7
lisy0123 <leesese3320@gmail.com>
2024-12-15 23:28:38 +0900

    testing google sigin in, done with ios

--- lib/env.dart
+++
@@ -1,4 +0,0 @@
-class Env {
-  static const projectUrl = String.fromEnvironment('PROJECT_URL');
-  static const apiKey = String.fromEnvironment('API_KEY');
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:provider/provider.dart';
-import 'package:puzzleeys_secret_letter/env.dart';
 import 'package:puzzleeys_secret_letter/providers/color_picker_provider.dart';
 import 'package:puzzleeys_secret_letter/providers/puzzle_scale_provider.dart';
 import 'package:puzzleeys_secret_letter/providers/writing_provider.dart';
@@ -12,7 +13,12 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Supabase.initialize(url: Env.projectUrl, anonKey: Env.apiKey);
+  await dotenv.load(fileName: ".env");
+  await Supabase.initialize(
+    url: dotenv.env['PROJECT_URL']!,
+    anonKey: dotenv.env['API_KEY']!,
+  );
+
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
   runApp(MultiProvider(
@@ -25,22 +31,95 @@ void main() async {
   ));
 }
 
+final supabase = Supabase.instance.client;
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(2340, 1080),
-      builder: (context, child) {
-        return MaterialApp(
-          theme: ThemeSetting.themeSetting(),
-          home: const Scaffold(
-            resizeToAvoidBottomInset: false,
-            body: MainScreen(),
-          ),
-        );
-      },
+    // return ScreenUtilInit(
+    //   designSize: const Size(2340, 1080),
+    //   builder: (context, child) {
+    //     return MaterialApp(
+    //       theme: ThemeSetting.themeSetting(),
+    //       home: const Scaffold(
+    //         resizeToAvoidBottomInset: false,
+    //         body: MainScreen(),
+    //       ),
+    //     );
+    //   },
+    // );
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String? _userId;
+
+  @override
+  void initState() {
+    super.initState();
+    supabase.auth.onAuthStateChange.listen((data) {
+      setState(() {
+        _userId = data.session?.user.id;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(_userId ?? 'Not signed in'),
+            ElevatedButton(
+              onPressed: () async {
+                /// Web Client ID that you registered with Google Cloud.
+                final webClientId = dotenv.env['WED_ID'];
+
+                /// iOS Client ID that you registered with Google Cloud.
+                final iosClientId = dotenv.env['IOS_ID'];
+                // Google sign in on Android will work without providing the Android
+                // Client ID registered on Google Cloud.
+                final GoogleSignIn googleSignIn = GoogleSignIn(
+                  clientId: iosClientId,
+                  serverClientId: webClientId,
+                );
+                final googleUser = await googleSignIn.signIn();
+                final googleAuth = await googleUser!.authentication;
+                final accessToken = googleAuth.accessToken;
+                final idToken = googleAuth.idToken;
+
+                if (accessToken == null) {
+                  throw 'No Access Token found.';
+                }
+                if (idToken == null) {
+                  throw 'No ID Token found.';
+                }
+
+                await supabase.auth.signInWithIdToken(
+                  provider: OAuthProvider.google,
+                  idToken: idToken,
+                  accessToken: accessToken,
+                );
+              },
+              child: Text('Sign in with Google'),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
