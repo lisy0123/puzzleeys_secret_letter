feb8be6e1eb262b0fd17a358caeab0caa375c344
lisy0123 <leesese3320@gmail.com>
2025-03-20 16:06:17 +0900

    edit import files

--- lib/screens/bar/status_bar.dart
+++ lib/screens/bar/status_bar.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
-import 'package:puzzleeys_secret_letter/constants/vars.dart';
 import 'package:puzzleeys_secret_letter/providers/bead_provider.dart';
 import 'package:puzzleeys_secret_letter/providers/bar_provider.dart';
 import 'package:puzzleeys_secret_letter/styles/box_decorations.dart';
